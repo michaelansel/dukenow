9d719601a028281a0c73f4d44ad555db054d8a37
Michael Ansel <mra13@michael-laptop>
2009-03-09 10:37:43 -0400

    Fixed bug that only showed up when in production environment (accidentally used the output of a debug command)

--- app/models/eatery.rb
+++ app/models/eatery.rb
@@ -9,15 +9,15 @@ class Eatery < ActiveRecord::Base
       # If we are in a time period with special operating times
       if  operating_time.start <= at.to_date and
           operating_time.end >= at.to_date
-        RAILS_DEFAULT_LOGGER.info "Special Operating Time Period!"
-        RAILS_DEFAULT_LOGGER.info "Effective from #{operating_time.start.to_s}"
-        RAILS_DEFAULT_LOGGER.info "           to  #{operating_time.end.to_s}"
+        RAILS_DEFAULT_LOGGER.debug "Special Operating Time Period!"
+        RAILS_DEFAULT_LOGGER.debug "Effective from #{operating_time.start.to_s}"
+        RAILS_DEFAULT_LOGGER.debug "           to  #{operating_time.end.to_s}"
         open = false if open == nil
 
         # If the eatery is open right now according to the special operating hours
         if  operating_time.opensAt.time(at)  < at and
             operating_time.closesAt.time(at) > at  and
-            (1<<at.wday) & operating_time.daysOfWeek != 0 and
+            (1<<at.wday) & operating_time.daysOfWeek != 0
           RAILS_DEFAULT_LOGGER.debug "Opens At: #{operating_time.opensAt.time(at).to_s}"
           RAILS_DEFAULT_LOGGER.debug "Closes At: #{operating_time.closesAt.time(at).to_s}"
           open = true
