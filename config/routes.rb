d43d5878dd7d8613af6c681a8c999a0d4f62593e
Michael Ansel <mra13@michael-laptop>
2009-03-12 12:37:08 -0400

    Add Places scaffold

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,6 @@
 ActionController::Routing::Routes.draw do |map|
+  map.resources :places
+
   map.resources :special_operating_times
   map.resources :regular_operating_times
   map.resources :eateries
